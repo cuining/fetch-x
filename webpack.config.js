761fc216163b3a867861d9be35aac1139780feba
cuining <ning.cui@ele.me>
2017-04-17 15:31:05 +0800

    Fix

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './index.js',
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'index.js',
