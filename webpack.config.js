963c7e5f7c7b2f9a6a4616064694a97985eb8d08
cuining <ning.cui@ele.me>
2017-04-17 15:18:16 +0800

    Use webpack

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'index.js',
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin()
+  ],
+  devtool: 'source-map'
+};
